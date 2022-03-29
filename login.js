db14bce6f5416748308281dd9ae0753c34a0d03b
Rahul kumar <rahul850kr@gmail.com>
2022-03-29 23:37:44 +0530

    login

---
+++ login.js
@@ -0,0 +1,39 @@
+var logindata=JSON.parse(localStorage.getItem("signupdata"));
+
+document.querySelector("form").addEventListener("submit",login)
+
+function login()
+{
+    event.preventDefault();
+
+    var selectedemail=document.querySelector("#email").value ;
+    var selectedpassword=document.querySelector("#password1").value;
+
+    var flag=false;
+    for(var i=0;i<logindata.length;i++)
+    {
+        if(logindata[i].email==selectedemail && logindata[i].password==selectedpassword)
+        {
+            flag=true;
+            break;
+        }
+    }
+
+    if(flag)
+    {
+        
+        document.querySelector("#email").value="";
+        document.querySelector("#password1").value="";
+        alert("Login Successful");
+
+        // window.location.href="home.html";
+
+    }
+    else{
+        alert("Oops wrong details please try again");
+         
+        document.querySelector("#email").value="";
+        document.querySelector("#password1").value="";
+    }
+
+}
